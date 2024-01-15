14f60928e832b68a89eca0382e2b2b0b9d7fe523
cyrano21 <louiscyrano@gmail.com>
2024-01-15 20:15:33 +0100

    add eventlistner scrollY

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -24,7 +24,7 @@ const Sidebar = ({ token, setUserToken }) => {
       }
     };
     window.addEventListener("scroll", handlescroll);
-    return removeEventListener("scroll", handlescroll);
+    return () => removeEventListener("scroll", handlescroll);
   }, []);
 
   return (
